c96c7f371baf1c6c5e53d3a2bb6592146a039678
Alexander Chernous <Alex.Chernous@247.ai>
2019-03-14 17:01:22 -0400

    split into functions

--- tester.py
+++ tester.py
@@ -10,52 +10,60 @@ results = {
     '1.0': "\nIt seems to me your sentiment is positive :)\n"
 }
 
+def loadModel():
+    # load the model from disk
+    sentimentModel = pickle.load(open('Models/sentimentModelFinal.sav', 'rb'))
 
-# load the model from disk
-sentimentModel = pickle.load(open('Models/sentimentModelFinal.sav', 'rb'))
+    ## Load dataset
+    #data = 'emojis'
+    #url = f'Data/{data}Parameters.csv'
+    #names = ['pos_neg_ratio', 'sentiment']
+    #dataset = pandas.read_csv(url, names=names)
 
-# Load dataset
-data = 'emojis'
-url = f'Data/{data}Parameters.csv'
-names = ['pos_neg_ratio', 'sentiment']
-dataset = pandas.read_csv(url, names=names)
+    ## Split-out validation dataset
+    #array = dataset.values
+    ##print(array)
+    #X = array[:,0:1]
+    ##print(X)
+    #y = array[:,1]
 
-# Split-out validation dataset
-array = dataset.values
-#print(array)
-X = array[:,0:1]
-#print(X)
-Y = array[:,1]
+    return sentimentModel
 
-stop = False
 
-while(not stop):
-    parameters = []
+def test(sentimentModel):
 
-    user_input = input("\nType in a sentence and I'll try to analyze its sentiment:\n--> ")
+    stop = False
 
-    try:
-        pos_neg_ratio = SP.positiveEmojis(user_input)/SP.negativeEmojis(user_input)
-        if pos_neg_ratio > 2:
+    while(not stop):
+        parameters = []
+
+        user_input = input("\nType in a sentence and I'll try to analyze its sentiment:\n--> ")
+
+        try:
+            pos_neg_ratio = SP.positiveEmojis(user_input)/SP.negativeEmojis(user_input)
+            if pos_neg_ratio > 2:
+                pos_neg_ratio = 2
+
+        except:
             pos_neg_ratio = 2
 
-    except:
-        pos_neg_ratio = 2
+        parameters.append([pos_neg_ratio])
+        print(pos_neg_ratio)
 
-    parameters.append([pos_neg_ratio])
-    print(pos_neg_ratio)
+        sentimentPrediction = sentimentModel.predict(parameters)
 
-    sentimentPrediction = sentimentModel.predict(parameters)
+        # give prob for each class
+        print(sentimentModel.predict_proba(parameters))
 
-    # give prob for each class
-    print(sentimentModel.predict_proba(parameters))
-
-    #look up in dictionary for appropriate response
-    print(results[str(sentimentPrediction[0])])
+        #look up in dictionary for appropriate response
+        print(results[str(sentimentPrediction[0])])
 
 
-    _ = input("Would you like to keep going?\n--> ").lower()
+        _ = input("Would you like to keep going?\n--> ").lower()
 
-    stop = 'no' in _ or "'t" in _
+        stop = 'no' in _ or "'t" in _
 
 
+if __name__ == "__main__":
+    sentimentModel = loadModel()
+    test(sentimentModel)
--- train.py
+++ train.py
@@ -15,56 +15,68 @@ from sklearn.svm import SVC
 import pickle
 from StringProcessing import setUpTrainingSet
 
-'''
-automated processing of some sentences for training.
-
-this does sentiment identification automatically with a simple regex,
-which defeats the purpose of ML, but it's just a POC for myself for future
-analysis where I'll tag sentences manually and can't simply use regex.
-'''
-setUpTrainingSet()
 
 
 
-
-# Load dataset
-data = 'emojis'
-url = f'Data/{data}Parameters.csv'
-names = ['pos_neg_ratio', 'sentiment']
-dataset = pandas.read_csv(url, names=names)
-
-#visual plot
-scatter_matrix(dataset)
-plt.show()
-
-# Split-out validation dataset
-array = dataset.values
-#print(array)
-X = array[:,0:1]
-#print(X)
-Y = array[:,1]
-#print(Y)
-validation_size = 0.30
-seed = 7
-X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+def loadData():
 
 
-svm = SVC(gamma='scale', probability=True)
-svm.fit(X_train, Y_train)
-predictions = svm.predict(X_test)
-print(X_test)
-print(accuracy_score(Y_test, predictions))
-print(confusion_matrix(Y_test, predictions))
-print(classification_report(Y_test, predictions))
+    # Load dataset
+    data = 'emojis'
+    url = f'Data/{data}Parameters.csv'
+    names = ['pos_neg_ratio', 'sentiment']
+    dataset = pandas.read_csv(url, names=names)
 
-pickle.dump(svm, open('Models/sentimentModelTrain.sav', 'wb'))
+    #visual plot
+    scatter_matrix(dataset)
+    plt.show()
+
+    # Split-out validation dataset
+    array = dataset.values
+    #print(array)
+    X = array[:,0:1]
+    #print(X)
+    y = array[:,1]
+
+    return X, y
+
+def trainingModel(X, y):
+    #print(Y)
+    validation_size = 0.30
+    seed = 7
+    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, y, test_size=validation_size, random_state=seed)
 
 
-#FINALIZING MODEL
+    svm = SVC(gamma='scale', probability=True)
+    svm.fit(X_train, Y_train)
+    predictions = svm.predict(X_test)
+    print(X_test)
+    print(accuracy_score(Y_test, predictions))
+    print(confusion_matrix(Y_test, predictions))
+    print(classification_report(Y_test, predictions))
 
-finalModel = SVC(gamma='scale', probability=True)
-finalModel.fit(X,Y)
-
-pickle.dump(finalModel, open('Models/sentimentModelFinal.sav', 'wb'))
+    pickle.dump(svm, open('Models/sentimentModelTrain.sav', 'wb'))
 
 
+def finalModel(X, y):
+    #FINALIZING MODEL
+
+    finalModel = SVC(gamma='scale', probability=True)
+    finalModel.fit(X,y)
+
+    pickle.dump(finalModel, open('Models/sentimentModelFinal.sav', 'wb'))
+
+
+if __name__ == "__main__":
+    '''
+    automated processing of some sentences for training.
+
+    this does sentiment identification automatically with a simple regex,
+    which defeats the purpose of ML, but it's just a POC for myself for future
+    analysis where I'll tag sentences manually and can't simply use regex.
+    '''
+    setUpTrainingSet()
+
+    X, y = loadData()
+    trainingModel(X, y)
+    finalModel(X, y)
